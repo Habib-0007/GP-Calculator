36d7c2e20b97d5a79208ab601e58f64699139a9a
Habib-0007 <adebayohabib7@gmail.com>
2024-01-24 18:10:55 +0100

    more new changes

--- js/index.js
+++ js/index.js
@@ -9,21 +9,41 @@ function addNewCourse(event) {
 	var courseNameInputVal = courseNameInput.value;
 	var courseUnitInputVal = courseUnitInput.value;
 	var courseGradeInputVal = courseGradeInput.value;
-	var newCourse = document.createElement("div");
-	courses.appendChild(newCourse);
 
-	var courseName = document.createElement("h6");
-	courseName.classList.add("course_name");
-	courseName.textContent = courseNameInputVal;
-	var courseUnit = document.createElement("h6");
-	courseName.classList.add("course_unit");
-	courseUnit.textContent = `${courseUnitInputVal} Unit`;
-	var courseGrade = document.createElement("p");
-	courseName.classList.add("course_grade");
-	courseGrade.textContent = courseGradeInputVal;
-	newCourse.appendChild(courseName);
-	newCourse.appendChild(courseUnit);
-	newCourse.appendChild(courseGrade);
+	if (
+		courseNameInput.value == "" ||
+		courseUnitInput.value == "" ||
+		courseGradeInput.value == ""
+	) {
+		alert("Please, fill the input with the right value");
+	} else {
+		var newCourse = document.createElement("div");
+		courses.appendChild(newCourse);
+
+		var courseName = document.createElement("h6");
+		courseName.classList.add("course_name");
+		courseName.textContent = courseNameInputVal;
+		var courseUnit = document.createElement("h6");
+		courseName.classList.add("course_unit");
+		courseUnit.textContent = `${courseUnitInputVal} Unit`;
+		var courseGrade = document.createElement("p");
+		courseName.classList.add("course_grade");
+		courseGrade.textContent = courseGradeInputVal;
+		newCourse.appendChild(courseName);
+		newCourse.appendChild(courseUnit);
+		newCourse.appendChild(courseGrade);
+	}
+
+	/*	courseUnit.forEach(eachCourseUnit => {
+		console.log(eachCourseGrade);
+	});*/
+	var courseUnitNew = document.querySelectorAll(".course_unit");
+	console.log(
+		courseUnitNew.map(eachCourseUnit => {
+			/* console.log(eachCourseUnit.innerHTML); */
+			return eachCourseUnit;
+		})
+	);
 }
 
 addBtn.addEventListener("click", addNewCourse);
